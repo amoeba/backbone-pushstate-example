13836f7bc37e1fd5fbfbd52ead7bd3c325a372ed
Bryce Mecum <petridish@gmail.com>
2018-01-28 16:17:44 -0900

    Code golf node server

--- server.js
+++ server.js
@@ -4,14 +4,8 @@ const port = process.env.PORT || 3000;
 const app = express();
 const fs = require("fs");
 
-// serve static assets normally
 app.use(express.static(__dirname + "/dist"));
-
-// handle every other route with index.html, which will contain
-// a script tag to your application's JavaScript file(s).
 app.get("*", function(request, response) {
   response.sendFile(path.resolve(__dirname, "dist", "index.html"));
 });
-
 app.listen(port);
-console.log("server started on port " + port);
