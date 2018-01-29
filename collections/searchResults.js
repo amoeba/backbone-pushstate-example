9c0d6dae0407dab74cff7117351c76e6553f03b4
Bryce Mecum <petridish@gmail.com>
2018-01-28 23:40:50 -0900

    Parse response the backbone way

--- collections/searchResults.js
+++ collections/searchResults.js
@@ -4,23 +4,9 @@ import SearchResult from "../models/searchResult";
 const SearchResults = Backbone.Collection.extend({
   model: SearchResult,
   url:
-    "https://dev.nceas.ucsb.edu/knb/d1/mn/v2/query/solr/?q=*:*&fl=title&wt=json",
-  fetch: function(options) {
-    console.log("SearchResults fetch() called");
-    var collection = this;
-
-    $.ajax({
-      type: "GET",
-      url: this.url,
-      dataType: "json",
-      success: function(data) {
-        console.log(data);
-        // TODO: Error handling
-        collection.reset(data.response.docs);
-      }
-    });
-
-    return Backbone.Collection.prototype.fetch.call(this, options);
+    "https://dev.nceas.ucsb.edu/knb/d1/mn/v2/query/solr/?q=*:*+AND+formatType:METADATA&title:*&fl=title&wt=json",
+  parse: function(data) {
+    return data.response.docs;
   }
 });
 
